af859c8d862374303fa7e1b61cd289d31b3fe83c
Minjoong Kim <minjoong.slow@gmail.com>
2018-08-19 22:00:49 +0900

    소스 정리

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-var cors = require('cors');
-var createError = require('http-errors');
-var express = require('express');
-var logger = require('morgan');
+const cors = require('cors')
+const createError = require('http-errors')
+const express = require('express')
+const logger = require('morgan')
 
-var app = express();
+const app = express()
 
 // CORS 적용
-app.use(cors());
+app.use(cors())
 // Access Logging
-app.use(logger('dev'));
+app.use(logger('dev'))
 // Body Parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.json({message: err.message});
-});
+  res.status(err.status || 500)
+  res.json({message: err.message})
+})
 
-module.exports = app;
+module.exports = app
