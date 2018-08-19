71bf5df16002afdfc6e5a4ae5d023cf5cb2108fc
Minjoong Kim <minjoong.slow@gmail.com>
2018-08-20 02:23:26 +0900

    District의 모든 Club 정보를 가져올 때, 상세 정보를 가져오도록 수정

--- functions/club.js
+++ functions/club.js
@@ -2,79 +2,84 @@ const R = require('ramda')
 const findAClubPage = require('./findAClubPage')
 
 const club = async clubId => {
-  const $ = await findAClubPage(clubId)
-  const name = $('div.detailHead h1.title').text()
-  const clubIdentities = R.map(R.trim, R.split(',', $('.info dl:nth-child(1) dd').text()))
+  try {
+    const $ = await findAClubPage(clubId)
+    const name = $('div.detailHead h1.title').text()
+    const clubIdentities = R.map(R.trim, R.split(',', $('.info dl:nth-child(1) dd').text()))
 
-  const districtId = R.compose(
-    R.last,
-    R.split(/\s/),
-    R.nth(1)
-  )(clubIdentities)
+    const districtId = R.compose(
+      R.last,
+      R.split(/\s/),
+      R.nth(1)
+    )(clubIdentities)
 
-  const [divisionId, areaId] = R.compose(
-    R.splitAt(1),
-    R.last,
-    R.split(/\s/),
-    R.nth(2)
-  )(clubIdentities)
+    const [divisionId, areaId] = R.compose(
+      R.splitAt(1),
+      R.last,
+      R.split(/\s/),
+      R.nth(2)
+    )(clubIdentities)
 
-  const charterDate = $('.info dl:nth-child(2) dd').text()
-  const website = $('.info dl:nth-child(3) dt a').attr('href')
-  const facebook = $('.contactLinks li:nth-child(1) a').attr('href')
-  const email = R.replace('mailto:', '')($('.contactLinks li:nth-child(2) a').attr('href'))
+    const charterDate = $('.info dl:nth-child(2) dd').text()
+    const website = $('.info dl:nth-child(3) dt a').attr('href')
+    const facebook = $('.contactLinks li:nth-child(1) a').attr('href')
+    const email = R.replace('mailto:', '')($('.contactLinks li:nth-child(2) a').attr('href'))
 
-  const phone =
-    R.compose(
-      R.trim,
-      R.replace('Phone:', '')
-    )($('.contact-info-phone').text())
-
-  const meetingTimes =
-    R.compose(
-      R.trim,
-      R.replace('Meeting Times:', '')
-    )($('.contact-info-meeting-times').text())
-
-  const location =
-    R.map(
+    const phone =
       R.compose(
-        R.replace(/\s{2,}/g, ' '),
         R.trim,
-        R.replace('Location:', '')
-      )
-    )($('.contact-info-body')
-      .contents()
-      .map((i, elem) =>
-        $(elem).text())
-      .toArray())
+        R.replace('Phone:', '')
+      )($('.contact-info-phone').text())
 
-  const [, , venueLine1, , venueLine2, , address] = location
+    const meetingTimes =
+      R.compose(
+        R.trim,
+        R.replace('Meeting Times:', '')
+      )($('.contact-info-meeting-times').text())
 
-  const [latitude, longitude] =
-    R.compose(
-      R.map(Number),
-      R.split(','),
-      R.replace('https://maps.google.com/maps?daddr=', '')
-    )($('a:contains("Get Directions")').attr('href'))
+    const location =
+      R.map(
+        R.compose(
+          R.replace(/\s{2,}/g, ' '),
+          R.trim,
+          R.replace('Location:', '')
+        )
+      )($('.contact-info-body')
+        .contents()
+        .map((i, elem) =>
+          $(elem).text())
+        .toArray())
 
-  return {
-    id: clubId,
-    name,
-    districtId,
-    divisionId,
-    areaId,
-    charterDate,
-    website,
-    facebook,
-    email,
-    phone,
-    meetingTimes,
-    venueLine1,
-    venueLine2,
-    address,
-    latitude,
-    longitude
+    const [, , venueLine1, , venueLine2, , address] = location
+
+    const [latitude, longitude] =
+      R.compose(
+        R.map(Number),
+        R.split(','),
+        R.replace('https://maps.google.com/maps?daddr=', '')
+      )($('a:contains("Get Directions")').attr('href'))
+
+    return {
+      id: clubId,
+      name,
+      districtId,
+      divisionId,
+      areaId,
+      charterDate,
+      website,
+      facebook,
+      email,
+      phone,
+      meetingTimes,
+      venueLine1,
+      venueLine2,
+      address,
+      latitude,
+      longitude
+    }
+  } catch (err) {
+    console.error(`error occured in club : ${clubId}`)
+    return {id: clubId}
   }
 }
 
--- functions/clubsOfDistrict.js
+++ functions/clubsOfDistrict.js
@@ -1,9 +1,15 @@
 const R = require('ramda')
 const districtDashboardPage = require('./districtDashboardPage')
+const club = require('./club')
+
+const mergedClub = async c => {
+  const clubDetail = await club(c.id)
+  return R.merge(clubDetail, c)
+}
 
 const clubsOfDistrict = async districtId => {
   const $ = await districtDashboardPage(districtId)
-  return $('td.Grid_Title_top5.min280.crop').map((i, elem) => {
+  const clubs = $('td.Grid_Title_top5.min280.crop').map((i, elem) => {
     const name = $(elem).attr('title')
     const id = $(elem).children('span.redFont').text()
     const divisionAreaId = $(elem).closest('tbody').parent().closest('tbody').attr('id')
@@ -18,8 +24,13 @@ const clubsOfDistrict = async districtId => {
       R.split('_')
     )(divisionAreaId)
     const member = Number($(elem).parent().find('td.Grid_Table.title_gray>span').text())
+
     return {id, name, division, area, member}
   }).toArray()
+
+  const clubsPromises = R.map(mergedClub)(clubs)
+
+  return await Promise.all(clubsPromises)
 }
 
 module.exports = clubsOfDistrict
